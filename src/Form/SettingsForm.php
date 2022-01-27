dd2a5272a047cb0efc5b0f58f4f2efb531284447
Stanislav Kutasevits <stan@bellcom.dk>
2022-01-27 10:43:03 +0200

    RINGSTED-240 adding section pages settings

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -9,6 +9,7 @@ namespace Drupal\os2web_pagebuilder\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
 
 /**
  * OS2Web Pagbuilder settings form.
@@ -40,43 +41,40 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config(SettingsForm::$configName);
+
     $form['hide_os2web_contact_fields'] = [
       '#type' => 'checkbox',
       '#title' => t('Hide OS2Web Contact block fields'),
-      '#default_value' => $this->config(SettingsForm::$configName)
-        ->get('hide_os2web_contact_fields'),
+      '#default_value' => $config->get('hide_os2web_contact_fields'),
       '#description' => t('If checked, the OS2Web contact block related fields will be hidden on Indholdside edit form.'),
     ];
 
     $form['hide_os2web_paragraph_banner_field'] = [
       '#type' => 'checkbox',
       '#title' => t('Hide Banner paragraph reference field (Media tab)'),
-      '#default_value' => $this->config(SettingsForm::$configName)
-        ->get('hide_os2web_paragraph_banner_field'),
+      '#default_value' => $config->get('hide_os2web_paragraph_banner_field'),
       '#description' => t('If checked, Banner paragraph reference field will be hidden on Indholdside edit form.'),
     ];
 
     $form['hide_os2web_paragraph_title_field'] = [
       '#type' => 'checkbox',
       '#title' => t('Hide Title field in paragraphs'),
-      '#default_value' => $this->config(SettingsForm::$configName)
-        ->get('hide_os2web_paragraph_title_field'),
+      '#default_value' => $config->get('hide_os2web_paragraph_title_field'),
       '#description' => t('If checked, Title field will be hidden on paragraphs.'),
     ];
 
     $form['hide_os2web_paragraph_subtitle_field'] = [
       '#type' => 'checkbox',
       '#title' => t('Hide Subtitle field in paragraphs'),
-      '#default_value' => $this->config(SettingsForm::$configName)
-        ->get('hide_os2web_paragraph_subtitle_field'),
+      '#default_value' => $config->get('hide_os2web_paragraph_subtitle_field'),
       '#description' => t('If checked, Subtitle field will be hidden on paragraphs.'),
     ];
 
     $form['hide_os2web_content_page_section_tab'] = [
       '#type' => 'checkbox',
       '#title' => t('Hide Section tab'),
-      '#default_value' => $this->config(SettingsForm::$configName)
-        ->get('hide_os2web_content_page_section_tab'),
+      '#default_value' => $config->get('hide_os2web_content_page_section_tab'),
       '#description' => t('If checked, Section tab will be hidden on Indholdside edit form.'),
     ];
 
@@ -87,9 +85,21 @@ class SettingsForm extends ConfigFormBase {
         'section_keyword' => t('Section AND keyword'),
         'sections_parents_keyword' => t('Section AND siblings (terms belong to the same top level parent) AND keyword'),
       ],
-      '#default_value' => $this->config(SettingsForm::$configName)
-        ->get('os2web_related_links_block_reference_mode'),
+      '#default_value' => $config->get('os2web_related_links_block_reference_mode'),
       '#description' => t('How related links block should be generated'),
+    ];
+
+    $form['os2web_section_pages'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => t('Section pages'),
+      '#default_value' => ($config->get('os2web_section_pages')) ? Node::loadMultiple(array_column($config->get('os2web_section_pages'), 'target_id')) : NULL,
+      '#tags' => TRUE,
+      '#target_type' => 'node',
+      '#selection_handler' => 'default',
+      '#selection_settings' => [
+        'target_bundles' => ['os2web_page'],
+      ],
+      '#description' => t('List pages which will have "Intro and "Description" fields marked non-mandatory'),
     ];
 
     // Overriding transliteration.
